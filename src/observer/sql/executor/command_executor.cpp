9d989c25da238110d5ae19128845fb6ee4f8f605
io Hilbert <pantao@hnu.edu.cn>
2024-03-16 13:48:56 +0000

    commit message

--- src/observer/sql/executor/command_executor.cpp
+++ src/observer/sql/executor/command_executor.cpp
@@ -17,6 +17,7 @@ See the Mulan PSL v2 for more details. */
 #include "event/sql_event.h"
 #include "sql/executor/create_index_executor.h"
 #include "sql/executor/create_table_executor.h"
+#include "sql/executor/drop_table_executor.h"
 #include "sql/executor/desc_table_executor.h"
 #include "sql/executor/help_executor.h"
 #include "sql/executor/load_data_executor.h"
@@ -31,7 +32,7 @@ RC CommandExecutor::execute(SQLStageEvent *sql_event)
   Stmt *stmt = sql_event->stmt();
 
   switch (stmt->type()) {
-    
+
     case StmtType::DROP_TABLE: {
       DropTableExecutor executor;
       return executor.execute(sql_event);
